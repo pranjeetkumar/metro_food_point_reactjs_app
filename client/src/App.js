467f060f0c325a177efe2cbe43b23accaaaeb64f
pranjeetkumar <mr.pran950@gmail.com>
2023-10-19 17:55:04 +0530

    Create File (Dashboard.jsx , DBLeftSection.jsx . DBRightSection.jsx , DBHeader.jsx)

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Main, Login } from './containers';
+import { Main, Login, Dashboard } from './containers';
 import { getAuth } from 'firebase/auth';
 import { app } from "./config/firebase.config";
 import { useDispatch, useSelector } from 'react-redux';
@@ -47,6 +47,7 @@ const App = () => {
       <Routes>
         <Route path="/*" element = { <Main/> }/>
         <Route path="/login" element = { <Login/> }/>
+        <Route path="/dashboard/*" element = { <Dashboard/> }/>
       </Routes>
 
       {alert?.type && <Alert type={alert?.type} message={alert?.message}/>}
